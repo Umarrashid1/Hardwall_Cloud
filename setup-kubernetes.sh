4a2e62c1e53c338c714bc9dc816844a9273ebc6b
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-11-20 20:14:42 +0100

    aaa

--- setup-kubernetes.sh
+++ setup-kubernetes.sh
@@ -12,3 +12,6 @@ echo "Applying Kubernetes configurations..."
 kubectl apply -f ./kubernetes/main_yamls/
 echo "Minikube started and Kubernetes configurations applied successfully."
 kubectl get all
+
+sleep 5
+#kubectl port-forward service/backend-service 8080:3000
