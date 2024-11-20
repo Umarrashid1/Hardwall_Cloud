0a036cfb20fa6113f0ca29a3d908cc8cdb8fdeff
umarrashid <umarrashid@hotmail.dk>
2024-11-20 15:24:04 +0100

    changes

--- device.py
+++ device.py
@@ -132,7 +132,6 @@ async def monitor_usb_devices(websocket):
         device_info = get_device_info(device)
         if not device_info:
             continue
-
         # Send device info to backend
         try:
             await websocket.send(json.dumps(device_info))
