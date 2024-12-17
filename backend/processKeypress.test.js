60742e8f190ed475e89dc80117862d98920a65e4
umarrashid <umarrashid@hotmail.dk>
2024-12-17 17:54:59 +0100

    Cuberneticz

--- backend/processKeypress.test.js
+++ backend/processKeypress.test.js
@@ -7,17 +7,16 @@ global.frontendClient = { readyState: WebSocket.OPEN, send: (msg) => console.log
 
 // Example keypress data
 const sampleKeypressData = [
-    { timestamp: '2024-12-16 16:55:52.358', endpoint: 'EP81', data: ['0', '0', '0', '0', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:12.309', endpoint: 'EP81', data: ['0', '0', '20', '0', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:12.319', endpoint: 'EP81', data: ['0', '0', '0', '0', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:13.149', endpoint: 'EP81', data: ['0', '0', '16', '0', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:13.181', endpoint: 'EP81', data: ['0', '0', '16', '7', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:13.234', endpoint: 'EP81', data: ['0', '0', '7', '0', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:15.196', endpoint: 'EP81', data: ['0', '0', '16', '0', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:15.291', endpoint: 'EP81', data: ['0', '0', '0', '0', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:15.424', endpoint: 'EP81', data: ['0', '0', '16', '0', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:15.504', endpoint: 'EP81', data: ['0', '0', '0', '0', '0', '0', '0', '0'] },
-    { timestamp: '2024-12-17 16:25:24.470', endpoint: 'EP81', data: ['0', '0', '1a', '0', '0', '0', '0', '0'] }
+    { timestamp: '2024-12-16 16:55:52.358', endpoint: 'EP81', data: ['0', '0', '4', '0', '0', '0', '0', '0'] }, // 'A' pressed
+    { timestamp: '2024-12-17 16:25:12.309', endpoint: 'EP81', data: ['0', '0', '20', '0', '0', '0', '0', '0'] }, // 'Q' pressed
+    { timestamp: '2024-12-17 16:25:12.319', endpoint: 'EP81', data: ['0', '0', '0', '0', '0', '0', '0', '0'] },  // Released
+    { timestamp: '2024-12-17 16:25:13.149', endpoint: 'EP81', data: ['0', '0', '16', '0', '0', '0', '0', '0'] }, // Shift pressed
+    { timestamp: '2024-12-17 16:25:13.181', endpoint: 'EP81', data: ['0', '0', '16', '7', '0', '0', '0', '0'] }, // Shift + D pressed
+    { timestamp: '2024-12-17 16:25:13.234', endpoint: 'EP81', data: ['0', '0', '7', '0', '0', '0', '0', '0'] },  // 'D' pressed
+    { timestamp: '2024-12-17 16:25:15.196', endpoint: 'EP81', data: ['0', '0', '20', '0', '0', '0', '0', '0'] }, // 'Q' pressed
+    { timestamp: '2024-12-17 16:25:15.291', endpoint: 'EP81', data: ['0', '0', '0', '0', '0', '0', '0', '0'] },  // Released
+    { timestamp: '2024-12-17 16:25:15.424', endpoint: 'EP81', data: ['0', '0', '16', '0', '0', '0', '0', '0'] }, // Shift pressed
+    { timestamp: '2024-12-17 16:25:15.504', endpoint: 'EP81', data: ['0', '0', '0', '0', '0', '0', '0', '0'] }   // Released
 ];
 
 // Call the function directly
